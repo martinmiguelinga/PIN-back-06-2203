f0f2954400526ce0df2712a8be388e0f319938fb
Martín Miguel Inga <martinmiguelinga@gmail.com>
2023-02-24 21:17:10 -0300

    Base de datos sin mailtrap

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contacto;
+use Illuminate\Http\Request;
+
+class ContactoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'string', 'email'],
+            'phone' => ['required', 'integer'],
+            'message' => ['required']
+
+
+        ]);
+
+        $contacto = Contacto::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'phone' => $request['phone'],
+            'message' => $request['message']
+        ]);
+        // ARMAR AQUI LA ESTRUCTURA PARA ENVIAR MAIL
+
+        return response()->json([
+            'mensaje' => 'Se Agrego Correctamente el Contacto',
+            'data' => $contacto,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contacto  $contacto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contacto $contacto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Contacto  $contacto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contacto $contacto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contacto  $contacto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contacto $contacto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contacto  $contacto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contacto $contacto)
+    {
+        //
+    }
+}
